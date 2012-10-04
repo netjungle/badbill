835ba06824bcd6982bd84c51da02944747bf2139
Hummel@Mac <dennis@netjungle.de>
2012-10-05 00:53:21 +0200

    including InvoicePositions

--- lib/badbill.rb
+++ lib/badbill.rb
@@ -10,6 +10,7 @@ require_relative 'badbill/base_resource'
 
 require_relative 'badbill/client'
 require_relative 'badbill/invoice'
+require_relative 'badbill/invoice_position'
 
 # Handles the connection and requests to the Billomat API.
 #
