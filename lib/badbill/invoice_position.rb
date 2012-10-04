a041c75f947d1b33df8339eac6757848161c6477
Hummel@Mac <dennis@netjungle.de>
2012-10-05 00:46:50 +0200

    added InvoicePositions - addding new resources is really easy ;)

---
+++ lib/badbill/invoice_position.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+class BadBill
+  # The clients resource handles all clients.
+  #
+  # See http://www.billomat.com/en/api/clients/
+  class InvoicePosition < BaseResource
+    
+  end
+end
