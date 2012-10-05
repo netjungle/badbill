dc3c14b13a9a7c493e923c59335d45f86d0af555
Hummel@Mac <dennis@netjungle.de>
2012-10-05 15:29:10 +0200

    => invoiceitem

--- lib/badbill/invoiceitem.rb
+++ lib/badbill/invoiceitem.rb
@@ -4,12 +4,8 @@ class BadBill
   # The clients resource handles all clients.
   #
   # See http://www.billomat.com/en/api/clients/
-  class InvoiceItem < BaseResource
+  class Invoiceitem < BaseResource
     
-    
-    def self.resource_name_singular
-      "invoiceitem"
-    end
     
     def self.resource_name
       "invoice-items"
